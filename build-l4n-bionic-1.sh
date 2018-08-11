8989947a3b4fbda74b5765b8bc2fd3e33ce98f7f
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-08-11 10:35:21 +0900

    update keyserver for neurodebian

--- build-l4n-bionic-1.sh
+++ build-l4n-bionic-1.sh
@@ -69,9 +69,9 @@ do
 done
 
 #Signature for neurodebian
-#sudo apt-key adv --recv-keys --keyserver \
-#     hkp://pgp.mit.edu:80 0xA5D32F012649A5A9
-sudo apt-key add neuro.debian.net.asc
+sudo apt-key adv --recv-keys --keyserver \
+     hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+#sudo apt-key add neuro.debian.net.asc
 
 #Installation of XFCE 4.12
 echo "Installation of XFCE 4.12"
