ff88ccbb7a6104c3057994653e5fbcfddffa1e85
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-08-11 14:55:16 +0900

    modify afni-related scripts

--- installer-scripts/afni_installer_bionic_2.sh
+++ installer-scripts/afni_installer_bionic_2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#AFNI Installer
-#This scripts install AFNI in /usr/local/AFNIbin
+#AFNI Installer part 2
+#This scripts install R AFNI in /usr/local/AFNIbin
 
 #This is based on https://afni.nimh.nih.gov/pub/dist/doc/htmldoc/background_install/install_instructs/steps_linux_ubuntu16.html
 
-#24 Dec 2017 K. Nemoto
+#11 Aug 2018 K. Nemoto
 
 cd $HOME
 
@@ -15,33 +15,12 @@ mkdir $R_LIBS
 echo 'export R_LIBS=$HOME/R' >> ~/.bashrc
 #curl -O https://afni.nimh.nih.gov/pub/dist/src/scripts_src/@add_rcran_ubuntu.tcsh
 #sudo tcsh @add_rcran_ubuntu.tcsh
-tcsh /usr/local/AFNIbin/rPkgsInstall -pkgs ALL
-
-#Make AFNI/SUMA profiles
-cp /usr/local/AFNIbin/AFNI.afnirc $HOME/.afnirc
-suma -update_env
-
-#Install help
-apsearch -update_all_afni_help
-
-cat << EOS >> ~/.bashrc
-
-ahdir=`apsearch -afni_help_dir`
-if [ -f "$ahdir/all_progs.COMP.bash" ]
-then
-   . $ahdir/all_progs.COMP.bash
-fi
-EOS
-
+rPkgsInstall -pkgs ALL
 
 #Evaluate setup
 cd $HOME
 echo "Check installation"
 afni_system_check.py -check_all
-
-#make icon show in the neuroimaging directory
-sed -i 's/NoDisplay=true/NoDisplay=false/' ~/.local/share/applications/afni.desktop
-
 
 echo "Finished"
 sleep 5
