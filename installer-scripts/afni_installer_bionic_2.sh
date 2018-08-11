d9452f4b9f27c32b90384f6f2c5d02053fbee438
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-08-11 13:33:56 +0900

    minor modification to afni_installer_bionic_2.sh

--- installer-scripts/afni_installer_bionic_2.sh
+++ installer-scripts/afni_installer_bionic_2.sh
@@ -15,7 +15,7 @@ mkdir $R_LIBS
 echo 'export R_LIBS=$HOME/R' >> ~/.bashrc
 #curl -O https://afni.nimh.nih.gov/pub/dist/src/scripts_src/@add_rcran_ubuntu.tcsh
 #sudo tcsh @add_rcran_ubuntu.tcsh
-rPkgsInstall -pkgs ALL
+/usr/local/AFNIbin/rPkgsInstall -pkgs ALL
 
 #Make AFNI/SUMA profiles
 cp /usr/local/AFNIbin/AFNI.afnirc $HOME/.afnirc
