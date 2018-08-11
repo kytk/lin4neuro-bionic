b235f6de0e869880d419ac94a9a2957351915c33
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-08-11 14:03:23 +0900

    bugfix for AFNI installer

--- installer-scripts/afni_installer_bionic_1.sh
+++ installer-scripts/afni_installer_bionic_1.sh
@@ -5,7 +5,7 @@
 
 #This is based on https://afni.nimh.nih.gov/pub/dist/doc/htmldoc/background_install/install_instructs/steps_linux_ubuntu16.html
 
-#24 Dec 2017 K. Nemoto
+#11 Aug 2018 K. Nemoto
 
 #Install prerequisite packages
 sudo apt update
@@ -17,7 +17,7 @@ sudo apt install -y libglu1-mesa-dev libglw1-mesa     \
                     libxm4 build-essential
 
 #Download AFNI binary and installer
-cd $HOME
+cd $HOME/Downloads
 
 curl -O https://afni.nimh.nih.gov/pub/dist/tgz/linux_ubuntu_16_64.tgz
 curl -O https://afni.nimh.nih.gov/pub/dist/bin/linux_ubuntu_16_64/@update.afni.binaries
@@ -25,13 +25,34 @@ curl -O https://afni.nimh.nih.gov/pub/dist/bin/linux_ubuntu_16_64/@update.afni.b
 #Install to /usr/local/AFNIbin
 sudo tcsh @update.afni.binaries -local_package linux_ubuntu_16_64.tgz -bindir /usr/local/AFNIbin -do_extras
 
+cd $HOME
+
 #.bashrc
 echo ' ' >> ~/.bashrc
 echo '#AFNI' >> ~/.bashrc
 echo 'export PATH=$PATH:/usr/local/AFNIbin' >> ~/.bashrc
 
+#Make AFNI/SUMA profiles
+cp /usr/local/AFNIbin/AFNI.afnirc $HOME/.afnirc
+suma -update_env
+
+#Install help
+apsearch -update_all_afni_help
+
+cat << EOS >> ~/.bashrc
+
+ahdir=`apsearch -afni_help_dir`
+if [ -f "$ahdir/all_progs.COMP.bash" ]
+then
+   . $ahdir/all_progs.COMP.bash
+fi
+EOS
+
+#make icon show in the neuroimaging directory
+sed -i 's/NoDisplay=true/NoDisplay=false/' ~/.local/share/applications/afni.desktop
+
 #Reboot
-echo "Please reboot the system"
+echo "Installation finished. Please reboot the system"
 sleep 5
 exit
 
